32d3f4e0f5cad88b1770e4cea0fab2d15434dbd0
VincenT <120357337+VT04@users.noreply.github.com>
2024-11-08 01:37:27 +1000

    Update Baseurl.js

--- Assignment1/UI/src/Baseurl.js
+++ Assignment1/UI/src/Baseurl.js
@@ -1,4 +1,4 @@
-const baseURL = 'http://ec2-3-106-139-108.ap-southeast-2.compute.amazonaws.com:5000';
+const baseURL = 'http://ec2-52-62-98-214.ap-southeast-2.compute.amazonaws.com:5000';
 
 module.exports = {
     baseURL
